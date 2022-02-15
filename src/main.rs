946bc3c2895f924e3c5afe0bde7154e5590cf5bf
mS <natsishere@gmail.com>
2022-02-15 18:18:47 +0100

    forgot to convert the char array to a string array lol

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io;
+use std::{thread, time::Duration};
 
 fn main() {
-    let x = ['h', 'i', 'd', 'e', 'a','k','i'];
+
+    let mut x = ['h', 'i', 'd', 'e', 'a','k','i'];
     let finish = ["Correct password !", "Incorrect password"];
 
     let mut guessing = String::new();
@@ -10,9 +12,13 @@ fn main() {
 
     let guessing: &str = guessing.trim();
 
+    let x: String = x.iter().collect();
+
     if !guessing.contains(&x[0..6]) {
-        println!("{}", finish[1])
+        println!("{}", finish[1]);
+        thread::sleep(Duration::from_millis(4000));
     } else {
-        println!("{}", finish[0])
+        println!("{}", finish[0]);
+        thread::sleep(Duration::from_millis(4000));
     }
 }
