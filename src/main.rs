1ae75b93f2ff6614377051f8c6cc18c4dad2a79d
Nats <natsishere@gmail.com>
2022-02-16 04:32:09 +0100

    better version lol :)

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,12 @@ use std::io;
 use std::{thread, time::Duration};
 
 fn main() {
-    let x = ['h', 'i', 'd', 'e', 'a','k','i']; 
-    let finish = ["Correct password !", "Incorrect password !"]; 
-
-    let mut guessing = String::new(); 
-
-    io::stdin().read_line(&mut guessing).expect("Can't read line"); 
-
-    let guessing: &str = guessing.trim(); 
-
-    let x: String = x.iter().collect(); 
-
-    if !guessing.contains(&x[0..6]) { 
-        println!("{}", finish[1]); 
-        thread::sleep(Duration::from_millis(4000));
-    } else { 
-        println!("{}", finish[0]); 
-        thread::sleep(Duration::from_millis(4000)); 
-    }
+    let x = ['h', 'i', 'd', 'e', 'a','k','i'];
+    let finish = ["Incorrect password !", "Correct password !"];
+    let mut guessing = String::new();
+    io::stdin().read_line(&mut guessing).expect("Can't read line");
+    let guessing: &str = guessing.trim();
+    let x: String = x.iter().collect();
+    println!("{}", finish[(guessing.eq(&x[0..7]) as usize)]);
+    thread::sleep(Duration::from_millis(2000))
 }
